77fdbb79f7f5da02da6cf1721f92c92d35417f05
lincerely <lincerely@gmail.com>
2019-03-08 02:17:35 +0800

    init commit

---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	if len(os.Args) < 3 {
+		fmt.Printf("Usage:\t %s file1 file2\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	filepathA := os.Args[1]
+	filepathB := os.Args[2]
+
+	fileA, err := os.Open(filepathA)
+	checkErr(err)
+
+	fileB, err := os.Open(filepathB)
+	checkErr(err)
+
+	//look for similarity in table id
+	readerA := csv.NewReader(bufio.NewReader(fileA))
+	readerB := csv.NewReader(bufio.NewReader(fileB))
+
+	dataA, err := readerA.ReadAll()
+	checkErr(err)
+
+	dataB, err := readerB.ReadAll()
+	checkErr(err)
+
+	fmt.Println("Matching Headers")
+	var headerA2B []int
+	for idxA, hA := range dataA[0] {
+		headerA2B = append(headerA2B, -1)
+		for idxB, hB := range dataB[0] {
+			if hA == hB {
+				headerA2B[idxA] = idxB
+				continue
+			}
+		}
+	}
+
+	fmt.Println("Getting keys")
+
+	var keyA []string
+	var keyB = make(map[string]int, 0)
+
+	//get uid for each row
+	for _, valAs := range dataA {
+		var uid = ""
+		for colIdxA, valA := range valAs {
+			if headerA2B[colIdxA] >= 0 {
+				uid += valA
+			}
+		}
+		keyA = append(keyA, uid)
+	}
+
+	for rowIdxB, valBs := range dataB {
+		var uid = ""
+		for _, v := range headerA2B {
+			if v >= 0 {
+				uid += valBs[v]
+			}
+		}
+		keyB[uid] = rowIdxB
+	}
+
+	fmt.Println("Mapping")
+	//map
+	var out [][]string
+	for rowIdxA, valAs := range dataA {
+		var outRow = valAs
+		for colIdxB, valB := range dataB[keyB[keyA[rowIdxA]]] {
+			if colIdxB > 0 {
+				outRow = append(outRow, valB)
+			}
+		}
+		out = append(out, outRow)
+	}
+
+	w := csv.NewWriter(os.Stdout)
+	w.WriteAll(out)
+
+	err = w.Error()
+	checkErr(err)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
